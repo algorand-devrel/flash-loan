255d331111de8d295374188109a5008b96345cf5
CiottiGiorgio <giorgio.ciotti@algorand.foundation>
2023-09-26 14:16:56 +0200

    Added withdrawal test.

--- __test__/flash-loan.test.ts
+++ __test__/flash-loan.test.ts
@@ -13,19 +13,32 @@ let appClient: FlashLoanClient;
 describe('FlashLoan', () => {
   let algod: algosdk.Algodv2;
   let sender: algosdk.Account;
+  let borrower: algosdk.Account;
   beforeEach(fixture.beforeEach);
 
   beforeAll(async () => {
     await fixture.beforeEach();
     algod = fixture.context.algod;
     const { testAccount, kmd } = fixture.context;
-    sender = await algokit.getOrCreateKmdWalletAccount({
-      name: 'flash-loan-sender',
-    }, algod, kmd);
-    await algokit.ensureFunded({
-      accountToFund: sender.addr,
-      minSpendingBalance: algokit.algos(1e6 + 10),
-    }, algod, kmd);
+
+    [sender, borrower] = await Promise.all([
+      algokit.getOrCreateKmdWalletAccount({
+        name: 'flash-loan-sender',
+      }, algod, kmd),
+      algokit.getOrCreateKmdWalletAccount({
+        name: 'flash-loan-borrower',
+      }, algod, kmd),
+    ]);
+    await Promise.all([
+      algokit.ensureFunded({
+        accountToFund: sender.addr,
+        minSpendingBalance: algokit.algos(1e6 + 10),
+      }, algod, kmd),
+      algokit.ensureFunded({
+        accountToFund: borrower.addr,
+        minSpendingBalance: algokit.microAlgos(2e6),
+      }, algod, kmd),
+    ]);
 
     appClient = new FlashLoanClient(
       {
@@ -50,5 +63,9 @@ describe('FlashLoan', () => {
       await algod.getTransactionParams().do(),
     );
     await appClient.optIn.deposit({ payment: deposit }, { sender });
+  });
+
+  test('withdraw', async () => {
+    await appClient.withdraw({ amount: 1e12 }, { sender });
   });
 });
